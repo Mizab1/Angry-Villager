356ff10410c1a6a866972145a4b4e67bc01cf4fe
Mizab <mizabmansari@gmail.com>
2024-06-25 21:59:50 +0530

    Modified the mod

--- dependencies/mods/Angry Villager Mod/Angry Villager Mod/src/main/java/me/mizab/angry_villager_mod/AngryVillagerMod.java
+++ dependencies/mods/Angry Villager Mod/Angry Villager Mod/src/main/java/me/mizab/angry_villager_mod/AngryVillagerMod.java
@@ -2,35 +2,31 @@ package me.mizab.angry_villager_mod;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.GameMode;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class AngryVillagerMod implements ModInitializer {
-    public static final Logger LOGGER = LoggerFactory.getLogger("generic");
 
     @Override
     public void onInitialize() {
-        LOGGER.info("FlyMod has been initialized!");
-
-        // Register the server tick event to check for player status
-        ServerTickEvents.END_WORLD_TICK.register(this::onEndWorldTick);
-    }
-
-    private void onEndWorldTick(ServerWorld world) {
-        for (ServerPlayerEntity player : world.getPlayers()) {
-            if (player.getCommandTags().contains("can_fly") || player.interactionManager.getGameMode() == GameMode.CREATIVE) {
-                player.getAbilities().allowFlying = true;
-                player.sendAbilitiesUpdate();
-            } else {
-                player.getAbilities().allowFlying = false;
-                player.sendAbilitiesUpdate();
-            }
-        }
+        // Register the event listener
+        ServerTickEvents.END_SERVER_TICK.register(server -> {
+            server.getPlayerManager().getPlayerList().forEach(player -> {
+                if (player.interactionManager.getGameMode() == GameMode.SURVIVAL && player.getCommandTags().contains("can_fly") && !player.getCommandTags().contains("flymod_processed")) {
+                    player.addCommandTag("flymod_processed");
+//                    player.sendMessage(Text.literal("You can fly"));
+                    player.getAbilities().allowFlying = true;
+                    player.sendAbilitiesUpdate();
+                } else if (player.interactionManager.getGameMode() == GameMode.SURVIVAL && !player.getCommandTags().contains("can_fly") && !player.getCommandTags().contains("flymod_processed")) {
+                    player.addCommandTag("flymod_processed");
+//                    player.sendMessage(Text.literal("You can't fly"));
+                    player.getAbilities().allowFlying = false;
+                    player.sendAbilitiesUpdate();
+                }
+            });
+        });
     }
 }
+
 
 
 
