6889fe7567defb92c2de6419c0dd4ee25ab43eb3
Mizab <mizabmansari@gmail.com>
2024-07-21 16:05:30 +0530

    Added cannon

---
+++ src/Attacks/Cannon/FaceAndFireCannon.ts
@@ -0,0 +1,100 @@
+import { Data, execute, loc, MCFunction, Objective, rel, scoreboard, Selector, summon, teleport } from "sandstone";
+import { self } from "../../Tick";
+import { cannonEntity } from "./PlaceCannon";
+
+// Constants to configure the cannon
+const FiringDistance: number = 30;
+const ActivationDistance: number = 40;
+
+// Create scores for storing the position
+const posX1 = Objective.create("pos_x1", "dummy")("@s");
+const posY1 = Objective.create("pos_y1", "dummy")("@s");
+const posZ1 = Objective.create("pos_z1", "dummy")("@s");
+
+const posX2 = Objective.create("pos_x2", "dummy")("@s");
+const posY2 = Objective.create("pos_y2", "dummy")("@s");
+const posZ2 = Objective.create("pos_z2", "dummy")("@s");
+
+MCFunction(
+  "attacks/cannon/face",
+  () => {
+    execute
+      .as(Selector("@e", { type: cannonEntity, tag: "cannon" }))
+      .at(self)
+      .run(() => {
+        // Kill the cannon if there is no ground
+        execute.if.block(rel(0, -0.35, 0), "#aestd1:air").run.kill(self);
+
+        // Face the cannon to the nearest player
+        execute
+          .facingEntity(Selector("@a", { distance: [Infinity, ActivationDistance], limit: 1, sort: "nearest" }), "eyes")
+          .run(() => {
+            teleport(self, rel(0, 0, 0), rel(0, 0));
+          });
+      });
+  },
+  {
+    runEach: "10t",
+  }
+);
+
+MCFunction(
+  "attacks/cannon/fire",
+  () => {
+    // Spawn the cannon ball
+    execute
+      .as(Selector("@e", { type: cannonEntity, tag: "cannon" }))
+      .at(self)
+      .anchored("eyes")
+      .positioned(loc(0, 0, 1))
+      .if(Selector("@a", { distance: [Infinity, FiringDistance] }))
+      .run(() => {
+        summon("minecraft:tnt", rel(0, 0, 0), {
+          Tags: ["cannon_ball"],
+          Fuse: 40,
+        });
+      });
+
+    // Fire the cannon
+    execute
+      .as(Selector("@e", { type: "minecraft:tnt", tag: "cannon_ball" }))
+      .at(self)
+      .rotatedAs(Selector("@e", { type: cannonEntity, tag: "cannon", limit: 1, sort: "nearest" }))
+      .run(() => {
+        // Variable that stores the cannon data
+        const cannonData = Data("entity", "@s");
+
+        // Store the initial position of the cannon ball
+        posX1.set(cannonData.select("Pos[0]"), 1000);
+        posY1.set(cannonData.select("Pos[1]"), 1000);
+        posZ1.set(cannonData.select("Pos[2]"), 1000);
+
+        // Teleport the cannon ball forward
+        teleport(self, loc(0, 0, 0.1));
+
+        // Store the final position of the cannon ball
+        posX2.set(cannonData.select("Pos[0]"), 1000);
+        posY2.set(cannonData.select("Pos[1]"), 1000);
+        posZ2.set(cannonData.select("Pos[2]"), 1000);
+
+        // Subtract the final position from the initial position
+        posX2["-="](posX1);
+        posY2["-="](posY1);
+        posZ2["-="](posZ1);
+
+        // Modify the motion data of the cannon ball to the new value
+        execute.store.result.entity(self, "Motion[0]", "double", 0.02).run(() => {
+          scoreboard.players.get(posX2.target, posX2.objective.name);
+        });
+        execute.store.result.entity(self, "Motion[1]", "double", 0.02).run(() => {
+          scoreboard.players.get(posY2.target, posY2.objective.name);
+        });
+        execute.store.result.entity(self, "Motion[2]", "double", 0.02).run(() => {
+          scoreboard.players.get(posZ2.target, posZ2.objective.name);
+        });
+      });
+  },
+  {
+    runEach: "50t",
+  }
+);
---
+++ src/Attacks/Cannon/PlaceCannon.ts
@@ -0,0 +1,22 @@
+import { ENTITY_TYPES, MCFunction, NBT, rel, summon } from "sandstone";
+
+export const cannonEntity: ENTITY_TYPES = "minecraft:zombie";
+
+MCFunction("attacks/cannon/place_cannon", () => {
+  // summon("minecraft:armor_stand", rel(0, 0, 0), {
+  //   Invisible: NBT.byte(1),
+  //   Tags: ["cannon"],
+  //   ArmorItems: [{}, {}, {}, { id: "minecraft:wooden_hoe", Count: NBT.byte(1), tag: { CustomModelData: 100002 } }],
+  // });
+
+  // summon zombie ~ ~ ~ {Silent:1b,DeathLootTable:"minecraft:bat",PersistenceRequired:1b,NoAI:1b,ArmorItems:[{},{},{},{id:"minecraft:wooden_hoe",Count:1b,tag:{CustomModelData:100002}}],ActiveEffects:[{Id:14,Amplifier:1b,Duration:999999,ShowParticles:0b}]}
+  summon(cannonEntity, rel(0, 0, 0), {
+    Tags: ["cannon"],
+    Silent: NBT.byte(1),
+    PersistenceRequired: NBT.byte(1),
+    NoAI: NBT.byte(1),
+    ActiveEffects: [{ Id: 14, Amplifier: NBT.byte(1), Duration: 999999, ShowParticles: NBT.byte(0) }],
+    ArmorItems: [{}, {}, {}, { id: "minecraft:wooden_hoe", Count: NBT.byte(1), tag: { CustomModelData: 100002 } }],
+    DeathLootTable: "minecraft:bat",
+  });
+});
