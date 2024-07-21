ad4dfbf4bcf6722aaddd21869bae86c1e20b9cbb
Mizab <mizabmansari@gmail.com>
2024-07-21 16:38:16 +0530

    Added Healer

---
+++ src/Attacks/HealingSpell/Tick.ts
@@ -0,0 +1,29 @@
+import { effect, execute, MCFunction, particle, rel, say, Selector } from "sandstone";
+import { self } from "../../Tick";
+
+// The effective distance within which healer can heal
+const healingDistance: number = 15;
+
+MCFunction(
+  "attacks/healing_spell/tick",
+  () => {
+    execute
+      .as(Selector("@e", { type: "minecraft:pillager", tag: "healer" }))
+      .at(self)
+      .run(() => {
+        // Display the particle
+        particle("minecraft:instant_effect", rel(0, 1, 0), [2, 0.5, 2], 0.5, 30, "force");
+
+        execute
+          .as(Selector("@e", { tag: "enemy", distance: [Infinity, healingDistance] }))
+          .at(self)
+          .run(() => {
+            effect.give(self, "minecraft:instant_health", 1, 0);
+            particle("minecraft:heart", rel(0, 1, 0), [0.5, 0.5, 0.5], 0.1, 5);
+          });
+      });
+  },
+  {
+    runEach: "4s",
+  }
+);
---
+++ src/Enemies/SummonHealer.ts
@@ -0,0 +1,13 @@
+import { MCFunction, NBT, rel, summon } from "sandstone";
+
+export const summonHealer = MCFunction("enemies/summon_healer", () => {
+  summon("minecraft:pillager", rel(0, 0, 0), {
+    PersistenceRequired: NBT.byte(1),
+    CanPickUpLoot: NBT.byte(0),
+    PatrolLeader: NBT.byte(0),
+    Patrolling: NBT.byte(0),
+    CanJoinRaid: NBT.byte(0),
+    Tags: ["enemy", "healer"],
+    HandItems: [{ id: "minecraft:splash_potion", Count: NBT.byte(1), tag: { Potion: "minecraft:healing" } }, {}],
+  });
+});
