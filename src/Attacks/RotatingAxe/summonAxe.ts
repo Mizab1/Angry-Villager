0caf16c19ae40f88362133a8405dfc512ff4e6a1
Mizab <mizabmansari@gmail.com>
2024-07-21 19:01:08 +0530

    Rename

--- src/Attacks/RotatingAxe/summonAxe.ts
+++ src/Attacks/RotatingAxe/summonAxe.ts
@@ -1,6 +1,6 @@
 import { MCFunction, NBT, rel, summon } from "sandstone";
 
-const summonRotatingAxe = MCFunction("attacks/rotating_axe/summon", () => {
+export const summonRotatingAxe = MCFunction("attacks/rotating_axe/summon", () => {
   summon("minecraft:armor_stand", rel(0, 0, 0), {
     NoGravity: NBT.byte(1),
     Invisible: NBT.byte(1),
@@ -13,9 +13,9 @@ const summonRotatingAxe = MCFunction("attacks/rotating_axe/summon", () => {
           left_rotation: [NBT.float(0), NBT.float(0), NBT.float(0), NBT.float(1)],
           right_rotation: [NBT.float(1), NBT.float(0), NBT.float(0), NBT.float(1)],
           translation: [NBT.float(0), NBT.float(0), NBT.float(0)],
-          scale: [NBT.float(1), NBT.float(1), NBT.float(1)],
+          scale: [NBT.float(0.5), NBT.float(0.5), NBT.float(0.5)],
         },
-        item: { id: "minecraft:iron_axe", Count: NBT.byte(1) },
+        item: { id: "minecraft:wooden_axe", Count: NBT.byte(1) },
       },
     ],
   });
