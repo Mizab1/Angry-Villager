471d434219bb559a643fe19efc701d3fdd2faf79
Mizab <mizabmansari@gmail.com>
2024-07-21 19:00:08 +0530

    SOC

--- src/Attacks/Tick.ts
+++ src/Attacks/Tick.ts
@@ -1,18 +1,21 @@
-import { execute, kill, MCFunction, raw, rel, Selector, teleport } from "sandstone";
+import { execute, MCFunction, Objective, rel, Selector, teleport } from "sandstone";
 import { self } from "../Tick";
+import { rotatingAxeTick } from "./RotatingAxe/Tick";
 
-export const AXE_TAG = "rotating_axe";
+// Create scores for storing the position
+export const posX1 = Objective.create("pos_x1", "dummy")("@s");
+export const posY1 = Objective.create("pos_y1", "dummy")("@s");
+export const posZ1 = Objective.create("pos_z1", "dummy")("@s");
+
+export const posX2 = Objective.create("pos_x2", "dummy")("@s");
+export const posY2 = Objective.create("pos_y2", "dummy")("@s");
+export const posZ2 = Objective.create("pos_z2", "dummy")("@s");
 
 MCFunction(
   "attacks/tick",
   () => {
-    // Teleport the rotating axe towards the player
-    execute
-      .as(Selector("@e", { type: "minecraft:armor_stand", tag: "axe_vehicle" }))
-      .at(self)
-      .run(() => {
-        raw(`teleport @s ^-0.4 ^ ^0.8 facing entity @p`);
-      });
+    // Tick function for rotating axe
+    rotatingAxeTick();
 
     // Rotate the entities that have rotate tag
     execute
@@ -20,16 +23,6 @@ MCFunction(
       .at(self)
       .run(() => {
         teleport(self, rel(0, 0, 0), rel(-40, 0));
-      });
-
-    // Register the damage if the rotating axe meets the player
-    execute
-      .as("@a")
-      .at(self)
-      .if(Selector("@e", { type: "minecraft:armor_stand", tag: AXE_TAG, distance: [Infinity, 2] }))
-      .run(() => {
-        kill(Selector("@e", { tag: AXE_TAG, distance: [Infinity, 4] }));
-        raw(`damage @s 2`);
       });
   },
   {
