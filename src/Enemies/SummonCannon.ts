f6da84d1a21fc953510a6c9a021ff0eab1cf61c0
Mizab <mizabmansari@gmail.com>
2024-07-24 14:51:16 +0530

    Changed file destination

--- src/Enemies/SummonCannon.ts
+++ src/Enemies/SummonCannon.ts
@@ -2,7 +2,7 @@ import { ENTITY_TYPES, MCFunction, NBT, rel, summon } from "sandstone";
 
 export const cannonEntity: ENTITY_TYPES = "minecraft:zombie";
 
-MCFunction("attacks/cannon/place_cannon", () => {
+const summonCannon = MCFunction("enemies/summon_cannon", () => {
   // summon("minecraft:armor_stand", rel(0, 0, 0), {
   //   Invisible: NBT.byte(1),
   //   Tags: ["cannon"],
@@ -11,7 +11,7 @@ MCFunction("attacks/cannon/place_cannon", () => {
 
   // summon zombie ~ ~ ~ {Silent:1b,DeathLootTable:"minecraft:bat",PersistenceRequired:1b,NoAI:1b,ArmorItems:[{},{},{},{id:"minecraft:wooden_hoe",Count:1b,tag:{CustomModelData:100002}}],ActiveEffects:[{Id:14,Amplifier:1b,Duration:999999,ShowParticles:0b}]}
   summon(cannonEntity, rel(0, 0, 0), {
-    Tags: ["cannon"],
+    Tags: ["cannon", "enemy"],
     Silent: NBT.byte(1),
     PersistenceRequired: NBT.byte(1),
     NoAI: NBT.byte(1),
