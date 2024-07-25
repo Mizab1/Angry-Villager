23139ee1900014437e888ce896bf964c446d4d04
Mizab <mizabmansari@gmail.com>
2024-07-25 17:07:53 +0530

    Added missile spawner enemy

---
+++ src/Enemies/SummonMissileThrower.ts
@@ -0,0 +1,13 @@
+import { MCFunction, NBT, rel, summon } from "sandstone";
+
+export const summonMissileThrower = MCFunction("enemies/summon_missile_thrower", () => {
+  summon("minecraft:vindicator", rel(0, 0, 0), {
+    PersistenceRequired: NBT.byte(1),
+    CanPickUpLoot: NBT.byte(0),
+    PatrolLeader: NBT.byte(0),
+    Patrolling: NBT.byte(0),
+    CanJoinRaid: NBT.byte(0),
+    Tags: ["enemy", "missile_thrower"],
+    Attributes: [{ Name: "generic.attack_damage", Base: 1 }],
+  });
+});
