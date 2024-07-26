88683b97f6eae17dec23c10099f440b5d241abb0
Mizab <mizabmansari@gmail.com>
2024-07-26 19:02:00 +0530

    Made it callable

--- src/KillAll.ts
+++ src/KillAll.ts
@@ -1,7 +1,7 @@
 import { execute, kill, MCFunction, Selector } from "sandstone";
 import { self } from "./Tick";
 
-MCFunction("kill_all", () => {
+export const killAllEnemy = MCFunction("kill_all", () => {
   kill(Selector("@e", { tag: ["enemy", "!no_kill_cmd"] }));
   execute
     .as(Selector("@e", { type: "minecraft:armor_stand", tag: "tf_catapult" }))
