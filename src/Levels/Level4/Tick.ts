89f909a66f15ea9f6b70fd9b8d021c154e96b93b
Mizab <mizabmansari@gmail.com>
2024-07-27 20:32:44 +0530

    Added lvl 4

---
+++ src/Levels/Level4/Tick.ts
@@ -0,0 +1,151 @@
+import { _, abs, clear, execute, give, item, MCFunction, raw, say, sleep, spawnpoint, teleport, tellraw, title } from "sandstone";
+import { giveLightningAbility } from "../../Abilities/Lightning/Give";
+import { summonEnchantedPillager } from "../../Enemies/SummonEnchantedPillager";
+import { summonNormalVindicator } from "../../Enemies/SummonNormalVindicator";
+import { enemyCounterScore, isStarted, levelCounterScore } from "../../Gameplay/Tick";
+import { killAllEnemy } from "../../KillAll";
+import { self } from "../../Tick";
+import { giveTornadoAbility } from "../../Abilities/Tornado/Give";
+import { summonFireWizard } from "../../Enemies/SummonFireWizard";
+
+// ! Change this according to the level
+// !! RENAME "startLevel" to the current level
+const levelStartCoords = abs(500, 150, 1500);
+const levelNumber = 4;
+const villageNumber = 2;
+const nextLevel = 0;
+
+const showTip = () => {
+  tellraw("@a", {
+    text: "TIP: You are now a giant and have unlocked the tornado ability, Use it to defend yourself",
+    color: "green",
+  });
+};
+
+const giveToolsToAllPlayers = MCFunction(`levels/village_${villageNumber}/level_${levelNumber}/give_tools_to_players`, () => {
+  give("@a", "minecraft:diamond_sword");
+  give("@a", "minecraft:crossbow");
+  give("@a", "minecraft:arrow", 20);
+
+  // Ability
+  execute.as("@a").run(() => giveLightningAbility());
+  execute.as("@a").run(() => giveTornadoAbility());
+
+  // Convert to giant
+  // ! MOD USED
+  execute.as("@a").run.raw(`scale set pehkui:base 3`);
+
+  // Give full set of armor
+  item.replace.entity("@a", "armor.head").with("minecraft:iron_helmet", 1);
+  item.replace.entity("@a", "armor.chest").with("minecraft:iron_chestplate", 1);
+  item.replace.entity("@a", "armor.legs").with("minecraft:iron_leggings", 1);
+  item.replace.entity("@a", "armor.feet").with("minecraft:iron_boots", 1);
+});
+
+const spawnEnemiesAtCoord = MCFunction(`levels/village_${villageNumber}/level_${levelNumber}/spawn_enemies`, () => {
+  const enemiesEnchantedPillagerSpawnCoords = [
+    abs(502, 150, 1520),
+    abs(512, 150, 1520),
+    abs(499, 150, 1486),
+    abs(487, 150, 1486),
+  ];
+  const enemiesEnchantedVindicatorSpawnCoords = [
+    abs(481, 150, 1492),
+    abs(482, 150, 1507),
+    abs(482, 150, 1523),
+    abs(483, 150, 1536),
+    abs(474, 150, 1540),
+  ];
+  const enemiesFireWizardSpawnCoords = [abs(468, 150, 1484), abs(466, 150, 1471), abs(466, 150, 1461)];
+
+  enemiesEnchantedPillagerSpawnCoords.forEach((coords) => {
+    execute.positioned(coords).run(() => {
+      summonEnchantedPillager();
+    });
+  });
+
+  enemiesEnchantedVindicatorSpawnCoords.forEach((coords) => {
+    execute.positioned(coords).run(() => {
+      summonNormalVindicator(6, "minecraft:iron_axe");
+    });
+  });
+
+  enemiesFireWizardSpawnCoords.forEach((coords) => {
+    execute.positioned(coords).run(() => {
+      summonFireWizard();
+    });
+  });
+});
+
+// ! Don't modify these
+export const startLevel4 = MCFunction(`levels/village_${villageNumber}/level_${levelNumber}/start`, async () => {
+  // Teleport player to the village
+  teleport("@a", levelStartCoords);
+
+  // Set the spawnpoint
+  spawnpoint("@a", levelStartCoords);
+
+  // Kill any residual mobs
+  execute.at("@a").run(() => killAllEnemy());
+
+  // Set the level counter
+  levelCounterScore.set(levelNumber);
+
+  await sleep("1s");
+
+  // Display the title of the level
+  title("@a").title([{ text: `Level ${levelNumber} Started`, color: "green" }]);
+
+  // Play the sound
+  execute.as("@a").at(self).run.playsound("minecraft:block.basalt.break", "master", self);
+
+  // Give tools to the player
+  giveToolsToAllPlayers();
+
+  // Spawn enemies
+  spawnEnemiesAtCoord();
+
+  // Set the level started variable
+  isStarted.set(1);
+
+  // Wait for 1 second
+  await sleep("3s");
+
+  // Give tips to the player
+  showTip();
+});
+
+const levelEndSequence = MCFunction(`levels/village_${villageNumber}/level_${levelNumber}/end`, async () => {
+  // Kill all the enemy in the level
+  killAllEnemy();
+
+  // Unset the level started variable
+  isStarted.set(0);
+
+  await sleep("1s");
+
+  // Display the title to all the player
+  title("@a").title({ text: "You have completed the level!", color: "gold" });
+  execute.as("@a").at(self).run.playsound("minecraft:ui.toast.challenge_complete", "master", self);
+  clear("@a");
+
+  await sleep("2s");
+
+  tellraw("@a", { text: "Initializing the next level", color: "red" });
+
+  await sleep("6s");
+
+  // Start the next level
+  // nextLevel();
+  say("Test Completed");
+});
+
+const checkAndEndLevel = MCFunction(
+  "levels/tick",
+  () => {
+    _.if(_.and(isStarted.equalTo(1), enemyCounterScore.equalTo(0), levelCounterScore.equalTo(levelNumber)), levelEndSequence);
+  },
+  {
+    onConflict: "append",
+  }
+);
