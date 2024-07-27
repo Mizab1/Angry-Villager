c1c8359459147080049abdf28e10de69f3e45e3e
Mizab <mizabmansari@gmail.com>
2024-07-27 20:17:19 +0530

    Commented kill

--- src/Tick.ts
+++ src/Tick.ts
@@ -9,8 +9,8 @@ MCFunction(
     effect.give("@a", "minecraft:saturation", 500, 0, true);
 
     // Kills villager and iron golem
-    kill(Selector("@e", { type: "minecraft:villager" }));
-    kill(Selector("@e", { type: "minecraft:iron_golem" }));
+    // kill(Selector("@e", { type: "minecraft:villager" }));
+    // kill(Selector("@e", { type: "minecraft:iron_golem" }));
   },
   {
     runEachTick: true,
