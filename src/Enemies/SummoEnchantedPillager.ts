a0f0937441c1d95a138504dd4140b8aaf8df6821
Mizab <mizabmansari@gmail.com>
2024-07-27 16:10:45 +0530

    Added new enemy

---
+++ src/Enemies/SummoEnchantedPillager.ts
@@ -0,0 +1,27 @@
+import { MCFunction, NBT, rel, summon } from "sandstone";
+
+export const summonNormalPillager = MCFunction("enemies/summon_normal_pillager", () => {
+  summon("minecraft:pillager", rel(0, 0, 0), {
+    CustomName: '{"text":"Soldier"}',
+    PersistenceRequired: NBT.byte(1),
+    CanPickUpLoot: NBT.byte(0),
+    PatrolLeader: NBT.byte(0),
+    Patrolling: NBT.byte(0),
+    CanJoinRaid: NBT.byte(0),
+    Tags: ["enemy", " normal_pillager"],
+    HandItems: [
+      {
+        id: "minecraft:crossbow",
+        Count: NBT.byte(1),
+        tag: {
+          Enchantments: [
+            { id: "minecraft:multishot", lvl: NBT.short(1) },
+            { id: "minecraft:piercing", lvl: NBT.short(1) },
+            { id: "minecraft:quick_charge", lvl: NBT.short(2) },
+          ],
+        },
+      },
+      {},
+    ],
+  });
+});
