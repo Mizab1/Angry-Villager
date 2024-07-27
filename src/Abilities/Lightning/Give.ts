0533f04480e9ec6c70248580e668b999c064e7bd
Mizab <mizabmansari@gmail.com>
2024-07-27 16:10:16 +0530

    Made it global

--- src/Abilities/Lightning/Give.ts
+++ src/Abilities/Lightning/Give.ts
@@ -2,7 +2,7 @@ import { MCFunction, NBT, give } from "sandstone";
 import { self } from "../../Tick";
 import { i } from "../../Utils/UtilFunctions";
 
-const giveLightningAbility = MCFunction("ability/lightning/give", () => {
+export const giveLightningAbility = MCFunction("ability/lightning/give", () => {
   give(
     self,
     i("minecraft:carrot_on_a_stick", {
