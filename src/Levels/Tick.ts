00b70b45a76ffb390048d304214a0828d8932651
Mizab <mizabmansari@gmail.com>
2024-07-28 23:10:57 +0530

    Added delayed enable flying

--- src/Levels/Tick.ts
+++ src/Levels/Tick.ts
@@ -1,15 +1,19 @@
-import { _, execute, MCFunction, Objective, raw, say } from "sandstone";
+import { _, MCFunction, raw } from "sandstone";
 import { isStarted, levelCounterScore } from "../Gameplay/Tick";
 
+MCFunction("levels/tick", () => {}, {
+  runEachTick: true,
+  onConflict: "append",
+});
+
 MCFunction(
-  "levels/tick",
+  "levels/enable_flying",
   () => {
     _.if(_.and(isStarted.equalTo(1), levelCounterScore.matches([8, 10])), () => {
       raw(`fly enable @a`);
     });
   },
   {
-    runEachTick: true,
-    onConflict: "append",
+    runEach: "10t",
   }
 );
