eae43666c752b7f796a5331580e4fc2d1dee8c99
Mizab <mizabmansari@gmail.com>
2024-07-28 18:39:12 +0530

    Reset the reach

--- src/Load.ts
+++ src/Load.ts
@@ -3,7 +3,8 @@ import { MCFunction, execute, raw, gamerule } from "sandstone";
 MCFunction(
   "load",
   () => {
-    execute.as("@a").run.raw(`scale reset`);
+    execute.as("@a").run.raw(`scale reset pehkui:base`);
+    execute.as("@a").run.raw(`scale reset pehkui:entity_reach`);
     raw(`scale persist set pehkui:base true @a`);
     gamerule("doFireTick", false);
     gamerule("commandBlockOutput", false);
