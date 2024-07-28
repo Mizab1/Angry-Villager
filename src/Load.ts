518acb91fd5b889e7dc19cb26cfd7c54d5cac1c1
Mizab <mizabmansari@gmail.com>
2024-07-28 18:59:09 +0530

    Disable the flight

--- src/Load.ts
+++ src/Load.ts
@@ -5,6 +5,7 @@ MCFunction(
   () => {
     execute.as("@a").run.raw(`scale reset pehkui:base`);
     execute.as("@a").run.raw(`scale reset pehkui:entity_reach`);
+    raw(`fly disable @a`);
     raw(`scale persist set pehkui:base true @a`);
     gamerule("doFireTick", false);
     gamerule("commandBlockOutput", false);
