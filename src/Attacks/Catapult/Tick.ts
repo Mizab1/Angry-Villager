abf1eba18b69e1a20c1a2089b31bb59f03d6366d
Mizab <mizabmansari@gmail.com>
2024-07-28 21:11:17 +0530

    Modified activation distance

--- src/Attacks/Catapult/Tick.ts
+++ src/Attacks/Catapult/Tick.ts
@@ -3,7 +3,7 @@ import { self } from "../../Tick";
 
 const isLoaded = Objective.create("ctplt_ld", "dummy")("@s");
 
-const firingDistance: Array<number> = [40, 45];
+const firingDistance: Array<number> = [10, 45];
 
 MCFunction(
   "attacks/catapult/load_or_fire",
@@ -17,7 +17,6 @@ MCFunction(
           .facingEntity(Selector("@a", { distance: [firingDistance[0], firingDistance[1]], limit: 1, sort: "nearest" }), "eyes")
           .run(() => {
             teleport(self, rel(0, 0, 0), ["~", "0"]);
-
             // Shoot or load
             _.if(isLoaded, () => {
               scoreboard.players.set("@s", "tfcp_anim", 10);
