bccaf8938629dff889912218c16286de4a379dd0
Mizab <mizabmansari@gmail.com>
2024-07-29 17:54:40 +0530

    Fixed typo

--- src/Gameplay/TextDisplays.ts
+++ src/Gameplay/TextDisplays.ts
@@ -12,7 +12,7 @@
 //     `summon text_display 2443 183 994 {billboard:"vertical",alignment:"center",Tags:["title_text"],text:'{"text":"Angry Villager","color":"gold","italic":false}'}`
 //   );
 //   raw(
-//     `summon text_display 2443 181 994 {billboard:"vertical",line_width:400,alignment:"center",Tags:["about_text"],text:'[{"text":"This map is about player who is fighting with villager. \\n","color":"white","italic":false},{"text":"The player will evolve and unlocked new abilities. \\n"},{"text":"The player will go from warrior to skywalker who can fly. \\n"},{"text":"At the same time, the villagers will also evolve and defend themselves. \\n"},{"text":"The player\'s goal is to destroy every threat using tools and powers.\\n"},{"text":"\\n"},{"text":"All the best!","color":"dark_aqua"}]'}`
+//     `summon text_display 2443 181 994 {billboard:"vertical",line_width:400,alignment:"center",Tags:["about_text"],text:'[{"text":"This map is about player who is fighting with the villagers. \\n","color":"white","italic":false},{"text":"The player will evolve and unlocked new abilities. \\n"},{"text":"The player will evolve from warrior to skywalker who can fly. \\n"},{"text":"At the same time, the villagers will also evolve and defend themselves. \\n"},{"text":"The player\'s goal is to destroy every threat using tools and powers.\\n"},{"text":"\\n"},{"text":"All the best!","color":"dark_aqua"}]'}`
 //   );
 
 //   // Set world spawn (Go on the coords)
