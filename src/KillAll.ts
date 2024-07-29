81c0b142e47404edd36ee7f3eebd5cb467e3b04a
Mizab <mizabmansari@gmail.com>
2024-07-29 15:14:09 +0530

    Added Meteor tag

--- src/KillAll.ts
+++ src/KillAll.ts
@@ -5,6 +5,7 @@ export const killAllEnemy = MCFunction("kill_all", () => {
   kill(Selector("@e", { tag: ["enemy", "!no_kill_cmd"] }));
   kill(Selector("@e", { tag: ["rotating_axe"] }));
   kill(Selector("@e", { type: "minecraft:fireball" }));
+  kill(Selector("@e", { type: "minecraft:armor_stand", tag: "falling_meteor" }));
   execute
     .as(Selector("@e", { type: "minecraft:armor_stand", tag: "tf_catapult" }))
     .at(self)
