5806cca59a81fd7c44ab7dc916f3e69e49d88cf8
Mizab <mizabmansari@gmail.com>
2024-07-29 14:51:31 +0530

    Added higkight trigger

--- src/Gameplay/Tick.ts
+++ src/Gameplay/Tick.ts
@@ -1,10 +1,14 @@
-import { MCFunction, Objective, scoreboard } from "sandstone";
+import { effect, execute, MCFunction, Objective, raw, scoreboard, Selector } from "sandstone";
 import { countEnemies } from "./EnemyCounter";
 
 // Side scores for information
 const gameScoreboard = Objective.create("game_scb", "dummy", { text: "Game", color: "gold" });
 export const enemyCounterScore = gameScoreboard("Enemy:");
 export const levelCounterScore = gameScoreboard("Level:");
+
+// Trigger score for highlighting
+const triggerHighlightScore = Objective.create("highlight", "trigger")("@s");
+const enableHighlightTrigger = () => raw(`scoreboard players enable @a highlight`);
 
 // Private Scoreboard
 const gameplayPrivateScoreboard = Objective.create("pvt_gmply", "dummy");
@@ -14,6 +18,7 @@ MCFunction(
   "gameplay/load",
   () => {
     scoreboard.objectives.setDisplay("sidebar", gameScoreboard.name);
+    enableHighlightTrigger();
   },
   {
     runOnLoad: true,
@@ -24,8 +29,20 @@ MCFunction(
   "gameplay/tick",
   () => {
     countEnemies();
+    highlightEnemies();
   },
   {
     runEachTick: true,
   }
 );
+
+const highlightEnemies = MCFunction("gameplay/highlight_enemies", () => {
+  execute
+    .as("@a")
+    .if(triggerHighlightScore.greaterThan(0))
+    .run(() => {
+      effect.give(Selector("@e", { tag: "enemy" }), "minecraft:glowing", 6, 1, false);
+      triggerHighlightScore.set(0);
+      enableHighlightTrigger();
+    });
+});
