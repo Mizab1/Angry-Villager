ef19d47e1685889350e8ccf4cbcb6587b75a901f
Mizab <mizabmansari@gmail.com>
2024-07-30 15:14:58 +0530

    Added automatic highlighting

--- src/Gameplay/Tick.ts
+++ src/Gameplay/Tick.ts
@@ -1,14 +1,10 @@
-import { effect, execute, MCFunction, Objective, raw, scoreboard, Selector } from "sandstone";
+import { effect, execute, MCFunction, Objective, raw, scoreboard, Selector, tellraw } from "sandstone";
 import { countEnemies } from "./EnemyCounter";
 
 // Side scores for information
 const gameScoreboard = Objective.create("game_scb", "dummy", { text: "Game", color: "gold" });
 export const enemyCounterScore = gameScoreboard("Enemy:");
 export const levelCounterScore = gameScoreboard("Level:");
-
-// Trigger score for highlighting
-const triggerHighlightScore = Objective.create("highlight", "trigger")("@s");
-const enableHighlightTrigger = () => raw(`scoreboard players enable @a highlight`);
 
 // Private Scoreboard
 const gameplayPrivateScoreboard = Objective.create("pvt_gmply", "dummy");
@@ -18,7 +14,6 @@ MCFunction(
   "gameplay/load",
   () => {
     scoreboard.objectives.setDisplay("sidebar", gameScoreboard.name);
-    enableHighlightTrigger();
   },
   {
     runOnLoad: true,
@@ -29,20 +24,19 @@ MCFunction(
   "gameplay/tick",
   () => {
     countEnemies();
-    highlightEnemies();
   },
   {
     runEachTick: true,
   }
 );
 
-const highlightEnemies = MCFunction("gameplay/highlight_enemies", () => {
-  execute
-    .as("@a")
-    .if(triggerHighlightScore.greaterThan(0))
-    .run(() => {
-      effect.give(Selector("@e", { tag: "enemy" }), "minecraft:glowing", 6, 1, false);
-      triggerHighlightScore.set(0);
-      enableHighlightTrigger();
-    });
-});
+MCFunction(
+  "gameplay/highlight_enemies",
+  () => {
+    effect.give(Selector("@e", { tag: "enemy" }), "minecraft:glowing", 6, 1, false);
+    tellraw("@a", { text: "All enemies are highlighted", color: "red" });
+  },
+  {
+    runEach: "60s",
+  }
+);
