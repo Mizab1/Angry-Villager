a7913017e295a956f3ba69c86d53c3d2e7688247
Mizab <mizabmansari@gmail.com>
2024-08-06 20:37:58 +0530

    Added zero drop chance and fixed the loot table

--- src/Enemies/SummonMissileLauncher.ts
+++ src/Enemies/SummonMissileLauncher.ts
@@ -5,11 +5,13 @@ const summonMissileLauncher = MCFunction("enemies/summon_missile_launcher", () =
     Tags: ["missile_launcher", "enemy"],
     PersistenceRequired: NBT.byte(1),
     ArmorItems: [{}, {}, {}, { id: "minecraft:wooden_hoe", Count: NBT.byte(1), tag: { CustomModelData: 100004 } }],
+    ArmorDropChances: [NBT.float(0.0), NBT.float(0.0), NBT.float(0.0), NBT.float(0.0)],
     Silent: NBT.byte(1),
     NoAI: NBT.byte(1),
     ActiveEffects: [{ Id: 14, Amplifier: NBT.byte(1), Duration: 999999, ShowParticles: NBT.byte(0) }],
     CustomName: '{"text":"Missile Launcher"}',
     Attributes: [{ Name: "generic.max_health", Base: 20 }],
     Health: NBT.float(20),
+    DeathLootTable: "minecraft:bat",
   });
 });
--- src/Enemies/SummonTank.ts
+++ src/Enemies/SummonTank.ts
@@ -7,6 +7,7 @@ const summonTank = MCFunction("enemies/summon_tank", () => {
     Health: NBT.float(40),
     Tags: ["tank", "enemy"],
     ArmorItems: [{}, {}, {}, { id: "minecraft:wooden_hoe", Count: NBT.byte(1), tag: { CustomModelData: 100003 } }],
+    ArmorDropChances: [NBT.float(0.0), NBT.float(0.0), NBT.float(0.0), NBT.float(0.0)],
     ActiveEffects: [{ Id: 14, Amplifier: NBT.byte(1), Duration: 999999, ShowParticles: NBT.byte(0) }],
     Attributes: [
       { Name: "generic.max_health", Base: 40 },
