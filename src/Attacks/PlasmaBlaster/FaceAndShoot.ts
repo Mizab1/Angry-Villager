d48967a9893301f667c7e08fb46b46338105248b
Mizab <mizabmansari@gmail.com>
2024-08-07 16:35:58 +0530

    Added blaster logic

---
+++ src/Attacks/PlasmaBlaster/FaceAndShoot.ts
@@ -0,0 +1,69 @@
+import { execute, loc, MCFunction, raw, rel, Selector, teleport } from "sandstone";
+import { raycast } from "sandstone-raycast";
+import { self } from "../../Tick";
+
+// Constants to configure the blaster
+const firingDistance: number = 30;
+const activationDistance: number = 40;
+
+MCFunction(
+  "attacks/plasma_blaster/face",
+  () => {
+    execute
+      .as(Selector("@e", { type: "minecraft:zombie", tag: "plasma_blaster" }))
+      .at(self)
+      .run(() => {
+        // Kill the blaster if there is no ground
+        execute.if.block(rel(0, -0.35, 0), "#aestd1:air").run.kill(self);
+
+        // Face the blaster to the nearest player
+        execute
+          .facingEntity(Selector("@a", { distance: [Infinity, activationDistance], limit: 1, sort: "nearest" }), "feet")
+          .run(() => {
+            teleport(self, rel(0, 0, 0), rel(0, 0));
+          });
+      });
+  },
+  {
+    runEach: "10t",
+  }
+);
+
+MCFunction(
+  "attacks/plasma_blaster/fire",
+  () => {
+    // Spawn the laser
+    execute
+      .as(Selector("@e", { type: "minecraft:zombie", tag: "plasma_blaster" }))
+      .at(self)
+      .if(Selector("@a", { distance: [Infinity, firingDistance] }))
+      .anchored("eyes")
+      .run(() => {
+        execute.positioned(loc(0, 0, 1)).run(() => {
+          shootLaserRaycast();
+        });
+      });
+  },
+  {
+    runEach: "50t",
+  }
+);
+
+const shootLaserRaycast = () => {
+  raycast(
+    "raycast/plasma_blaster/main",
+    // @ts-ignore
+    "#aestd1:passthrough",
+    null,
+    MCFunction("raycast/plasma_blaster/update", () => {
+      raw(`particle dust 0.239 0.392 1.000 1 ^0.8 ^ ^ 0 0 0 0 3 normal`);
+      raw(`particle dust 0.239 0.392 1.000 1 ^-0.8 ^ ^ 0 0 0 0 3 normal`);
+      execute.as(Selector("@a", { distance: [Infinity, 2] })).run(() => {
+        raw(`damage @s 4 minecraft:magic`);
+      });
+    }),
+    MCFunction("raycast/plasma_blaster/hit", () => {}),
+    0.5,
+    firingDistance
+  );
+};
