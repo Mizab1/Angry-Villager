bfb4c117f37ead2626e0d04186ee8ecc79146d8a
Mizab <mizabmansari@gmail.com>
2024-08-07 15:59:36 +0530

    Commented out code

--- src/Attacks/Missile/SpawnMissile.ts
+++ src/Attacks/Missile/SpawnMissile.ts
@@ -1,21 +1,21 @@
-import { execute, MCFunction, particle, rel, Selector } from "sandstone";
-import { self } from "../../Tick";
+// import { execute, MCFunction, particle, rel, Selector } from "sandstone";
+// import { self } from "../../Tick";
 
-MCFunction(
-  "attacks/spawn_missile",
-  () => {
-    execute
-      .as(Selector("@e", { type: "minecraft:vindicator", tag: "missile_thrower" }))
-      .at(self)
-      .run(() => {
-        // Display particle
-        particle("minecraft:campfire_cosy_smoke", rel(0, 1, 0), [0.5, 0.5, 0.5], 0.1, 6);
+// MCFunction(
+//   "attacks/spawn_missile",
+//   () => {
+//     execute
+//       .as(Selector("@e", { type: "minecraft:vindicator", tag: "missile_thrower" }))
+//       .at(self)
+//       .run(() => {
+//         // Display particle
+//         particle("minecraft:campfire_cosy_smoke", rel(0, 1, 0), [0.5, 0.5, 0.5], 0.1, 6);
 
-        // ! The implementation of missile is done using MCB language in MCB internal
-        execute.positioned(rel(0, 1, 0)).run.functionCmd("internal:missiles");
-      });
-  },
-  {
-    runEach: "180t",
-  }
-);
+//         // ! The implementation of missile is done using MCB language in MCB internal
+//         execute.positioned(rel(0, 1, 0)).run.functionCmd("internal:missiles");
+//       });
+//   },
+//   {
+//     runEach: "180t",
+//   }
+// );
