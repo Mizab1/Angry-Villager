2bb4504f110b562c1dd68816bd896998cee26209
Mizab <mizabmansari@gmail.com>
2024-08-07 20:24:32 +0530

    Summon function for soldiers

---
+++ src/Enemies/SummonFuturisticSoldier.ts
@@ -0,0 +1,15 @@
+import { MCFunction, NBT, rel, summon } from "sandstone";
+
+export const summonFuturisticSoldier = MCFunction("enemies/summon_futuristic_soldier", () => {
+  summon("minecraft:pillager", rel(0, 0, 0), {
+    DeathLootTable: "minecraft:bat",
+    CustomName: '{"text":"Futuristic Soldier"}',
+    PersistenceRequired: NBT.byte(1),
+    CanPickUpLoot: NBT.byte(0),
+    PatrolLeader: NBT.byte(0),
+    Patrolling: NBT.byte(0),
+    CanJoinRaid: NBT.byte(0),
+    Tags: ["enemy", " futuristic_soldier"],
+    HandItems: [{ id: "minecraft:crossbow", Count: NBT.byte(1), tag: { CustomModelData: 100002 } }, {}],
+  });
+});
---
+++ src/Enemies/SummonMilitarySoldier.ts
@@ -0,0 +1,15 @@
+import { MCFunction, NBT, rel, summon } from "sandstone";
+
+export const summonMilitarySoldier = MCFunction("enemies/summon_military_soldier", () => {
+  summon("minecraft:pillager", rel(0, 0, 0), {
+    DeathLootTable: "minecraft:bat",
+    CustomName: '{"text":"Military Soldier"}',
+    PersistenceRequired: NBT.byte(1),
+    CanPickUpLoot: NBT.byte(0),
+    PatrolLeader: NBT.byte(0),
+    Patrolling: NBT.byte(0),
+    CanJoinRaid: NBT.byte(0),
+    Tags: ["enemy", " military_soldier"],
+    HandItems: [{ id: "minecraft:crossbow", Count: NBT.byte(1), tag: { CustomModelData: 100001 } }, {}],
+  });
+});
