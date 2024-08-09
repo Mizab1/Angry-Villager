db955c2f3831d1d79915f05b524f54dddf163545
Mizab <mizabmansari@gmail.com>
2024-08-09 19:55:20 +0530

    Make the bossbar invisible

--- src/Reset.ts
+++ src/Reset.ts
@@ -1,7 +1,11 @@
-import { MCFunction } from "sandstone";
+import { bossbar, MCFunction } from "sandstone";
 import { enemyCounterScore, levelCounterScore } from "./Gameplay/Tick";
+import { bossbarName } from "./Gameplay/DivinityBar";
 
-MCFunction("reset_side_score", () => {
+MCFunction("reset", () => {
   enemyCounterScore.set(0);
   levelCounterScore.set(0);
+
+  //@ts-ignore
+  bossbar.set(bossbarName).visible(false);
 });
