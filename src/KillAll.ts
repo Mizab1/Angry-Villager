bfaa8833313673099f886e27d8ea75dd199cb5c2
Mizab <mizabmansari@gmail.com>
2024-08-09 17:13:18 +0530

    Added missile's tag

--- src/KillAll.ts
+++ src/KillAll.ts
@@ -7,6 +7,7 @@ export const killAllEnemy = MCFunction("kill_all", () => {
   kill(Selector("@e", { type: "minecraft:fireball" }));
   kill(Selector("@e", { type: "minecraft:armor_stand", tag: "falling_meteor" }));
   kill(Selector("@e", { type: "minecraft:armor_stand", tag: "pattern" }));
+  kill(Selector("@e", { type: "minecraft:armor_stand", tag: "missile" }));
   kill(Selector("@e", { type: "minecraft:bee", tag: "ld_drone_hitbox" }));
   kill(Selector("@e", { type: "minecraft:bee", tag: "ld_turret_hitbox" }));
   kill(Selector("@e", { type: "minecraft:horse", tag: "enemy_horse" }));
